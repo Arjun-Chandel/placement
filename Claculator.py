40a3f52916be7b5064ddf1204e3c74abbf17c79b
Arjun-Chandel <140698272+Arjun-Chandel@users.noreply.github.com>
2023-08-04 08:39:10 +0530

    Add files via upload

---
+++ Claculator.py
@@ -0,0 +1,28 @@
+print("       CALCULATOR       ")
+a=input("Enter first number:")
+b=input("Enter second number:")
+# print("For addition press \"z\"")
+# print("For substraction press \"x\"")
+# print("For multiplication press \"c\"")
+# print("For division press \"v\"")
+z=int(a)+int(b)
+x=int(a)-int(b)
+c=int(a)*int(b)
+v=int(a)/int(b)
+# l=int(input("enter the operation:"))
+# print(int(l))
+print("Addition =",z)
+print("Substraction =",x)
+print("Multiplication =",c)
+print("Division =",v)
+
+
+
+
+
+
+
+
+
+
+
